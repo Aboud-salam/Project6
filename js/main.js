fe64799d9fa56442f65152b40ed88620189ecb5e
Aboud A-Salam <ar.48.salam@gmail.com>
2023-07-05 04:02:37 +0300

    adding the project files

---
+++ js/main.js
@@ -0,0 +1,59 @@
+// nav toggle
+let toggleBtn = document.querySelector(".toggle-btn")
+let sideLinks = document.querySelector(".side-links")
+const mediaQuery = window.matchMedia('(max-width: 767px)')
+toggleBtn.onclick = function () {
+        sideLinks.classList.toggle("clicked")
+        if (sideLinks.classList.contains("clicked")) {
+            sideLinks.style.height = `336px`
+        } else {
+            sideLinks.style.height = `0px`
+        }
+}
+window.onresize = function() {
+    if (mediaQuery.matches === false) {
+        sideLinks.style.height = `0px`
+    }
+}
+// questions toggle
+let question = document.querySelectorAll(".asked-questions .container .questions .question")
+let content = document.querySelector(".asked-questions .container .questions .question .content")
+let clicks = 0;
+let getData = async () => {
+    let response = await fetch("answers.json")
+    let data = await response.json()
+    return data
+}
+getData().then(res => {
+    let answers = res;
+    question.forEach((e, index) => {
+        e.onclick = function () {
+            console.log(index)
+            e.classList.toggle("clicked")
+            if (e.classList.contains("clicked")) {
+                e.lastElementChild.innerHTML = "";
+                let answer = answers.filter(e => {
+                    return e[`answer-${index}`]
+                })
+                console.log(e.lastElementChild)
+                    let element = document.createElement("p")
+                    element.appendChild(document.createTextNode(answer[0][`answer-${index}`]))
+                    element.classList.add("description")
+                    e.lastElementChild.appendChild(element)
+                    e.lastElementChild.style.height = `${e.lastElementChild.scrollHeight}px`
+            } else {
+                e.lastElementChild.style.height = `0px`
+            }
+            removeClicked(index)
+        }
+    })
+})
+function removeClicked(indexOne) {
+    question.forEach((el, indexTwo) => {
+        let des = el.querySelector(".content")
+        if (indexOne != indexTwo) {
+            el.classList.remove("clicked")
+            des.style.height = `0px`
+        }
+    })
+}
